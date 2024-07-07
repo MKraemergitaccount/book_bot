36dd8628b77cc01e7555e9036af7c11d34684a26
maik <ii9516089@gmail.com>
2024-07-08 00:05:06 +0200

    Everything but not the sort ('yet')

--- book_bot.py
+++ book_bot.py
@@ -1,3 +1,5 @@
+
+
 class book_bot:
 
     _books = {
@@ -7,8 +9,10 @@ class book_bot:
     def __init__(self):
         self.book = None
         self.words = None
-        self.amount_letter = 0
-        self.amount_per_chars = {}
+        self.letters = []
+        self.amount_words = 0
+        self.amount_letters = 0
+        self.amount_per_chars_set = {}
 
 
     # Give the index of a book, receive its words in strings
@@ -18,30 +22,48 @@ class book_bot:
             self.words = book_contents.split()
 
 
-    # counts the amount of letters in the book.
+    # counts the amount of words in the book.
+    def count_words(self):
+        for word in self.words:
+            self.amount_words+=1
+
+
+    # counts the amount of chars in the book.
+    # sperates letters from words
     def count_letters(self):
         for word in self.words:
-            self.amount_letter+=1
+            for letter in word:
+                self.letters.append(letter)
+                self.amount_letters+=1
 
 
     # creates dict with all chars in book
     def which_chars_in_book(self):
         for word in self.words:
             for char in word:
-                self.amount_per_chars[char] = 0
+                if char.isalpha():
+                    self.amount_per_chars_set[char.lower()] = 0
 
 
-    # counts the amount of each char and updates amount_per_chars.
+    # counts the amount of each indiviual char and updates amount_per_chars.
     def count_characters(self):
-        for key, value in self.amount_per_chars.items():
+        for key ,value in self.amount_per_chars_set.items():
+            for letter in self.letters:
+                if key==letter:
+                    self.amount_per_chars_set[key]+=1
+    
 
     def Get_words(self):
         return self.words
 
 
+    def Get_amount_words(self):
+        return self.amount_words
+
+
     def Get_amount_letters(self):
-        return self.amount_letter
+        return self.amount_letters
 
-
+    
     def Get_amount_per_chars(self):
-        return self.amount_per_chars
+        return self.amount_per_chars_set
--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from book_bot import book_bot
 
 bb = book_bot()
 bb.read_book(1)
+bb.count_words()
 bb.count_letters()
 bb.which_chars_in_book()
 bb.count_characters()
 
-#print(bb.Get_amount_letters())
-print(bb.Get_amount_per_chars())
+print(f' There are {bb.Get_amount_letters()} in the book.')
+for letter, amount in bb.Get_amount_per_chars().items():
+    print(f'The {letter} character was found {amount} times.')
+
+
+
